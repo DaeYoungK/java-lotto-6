b9c6cbe0afd302606e8347cd8eb5aa61940f00e9
DaeYoungK <hanguk830@naver.com>
2023-11-26 23:55:02 +0900

    test(WinningNumber): 당첨번호와 보너스 번호를 받아서 저장하는 기능 테스트

---
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class WinningNumberTest {
+
+    WinningNumber winningNumber = new WinningNumber();
+
+    @DisplayName("쉼표로 구분된 문자열을 받아서 당첨 번호를 지정한다.")
+    @Test
+    void setWinningNumber() {
+        String numbers = "1,2,3,4,5,6";
+
+        winningNumber.setWinning(numbers);
+        List<Integer> winningNumbers = winningNumber.getWinning();
+
+        Assertions.assertThat(winningNumbers).containsExactly(1,2,3,4,5,6);
+        Assertions.assertThat(winningNumbers.size()).isEqualTo(6);
+    }
+
+    @DisplayName("번호를 받아서 보너스 번호를 지정한다.")
+    @Test
+    void setBonusNumber() {
+        String number = "7";
+
+        winningNumber.setBonus(number);
+        int bonus = winningNumber.getBonus();
+
+        Assertions.assertThat(bonus).isEqualTo(7);
+    }
+}
