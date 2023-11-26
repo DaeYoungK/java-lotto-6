6612e0766b281473be75c1b619e917ee2812723c
DaeYoungK <hanguk830@naver.com>
2023-11-27 00:22:58 +0900

    fix(LottoStore): 저장소에 로또 저장 후 반환하지 않도록 변경

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -8,11 +8,9 @@ public class LottoStore {
 
     private List<Lotto> lottos = new ArrayList<>();
 
-    public List<Integer> addLotto(List<Integer> numbers) {
+    public void addLotto(List<Integer> numbers) {
         Lotto lotto = Lotto.createLotto(numbers);
         lottos.add(lotto);
-
-        return lotto.getNumbers();
     }
 
     public List<Lotto> getLottos() {
