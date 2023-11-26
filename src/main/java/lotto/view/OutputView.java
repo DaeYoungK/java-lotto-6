0252adb236c1d7e8e22fc9a6f7f941b0df7ffe37
DaeYoungK <hanguk830@naver.com>
2023-11-27 00:43:28 +0900

    fix(OutputView): 구매한 로또 개수 출력 타입 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,7 @@ package lotto.view;
 
 public class OutputView {
 
-    private static final String PURCHASE_LOTTO = "%n개를 구매했습니다.";
+    private static final String PURCHASE_LOTTO = "%d개를 구매했습니다.";
     private static final String STATISTIC_MESSAGE = "당첨 통계\n---\n";
 
     public void printPurchaseLotto(int count, String history) {
