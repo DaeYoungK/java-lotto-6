bb3b2cbc8f034c7cd208bfcf2ac6ccb8caf8118c
DaeYoungK <hanguk830@naver.com>
2023-11-26 20:49:39 +0900

    feat(LottoStore): Get 메서드 추가

---
+++ src/main/java/lotto/domain/LottoStore.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoStore {
+
+    private List<Lotto> lottos = new ArrayList<>();
+
+    public void addLotto(List<Integer> numbers) {
+        Lotto lotto = Lotto.createLotto(numbers);
+        lottos.add(lotto);
+    }
+
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
+    }
+}
