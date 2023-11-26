f14aae6f4fa0d9ac9840fb53ceac920f28c3f5f8
DaeYoungK <hanguk830@naver.com>
2023-11-26 20:03:19 +0900

    feat(OutputView): 출력하는 기능 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+public class OutputView {
+
+    private static final String PURCHASE_LOTTO = "%s개를 구매했습니다.";
+    private static final String STATISTIC_MESSAGE = "당첨 통계\n---\n";
+
+    public void printPurchaseLotto(String count, String history) {
+        System.out.println(String.format(PURCHASE_LOTTO, count));
+        System.out.println(history);
+    }
+
+    public void printStatistic(String statistic) {
+        System.out.println(statistic);
+    }
+
+}
