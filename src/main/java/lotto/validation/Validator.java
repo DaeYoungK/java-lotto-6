b4cdb2d8109fb4c8cfd2a6e05f7335d815af7657
DaeYoungK <hanguk830@naver.com>
2023-11-29 01:46:44 +0900

    feat(Validator): 입력 받는 데이터 검증 기능 추가

---
+++ src/main/java/lotto/validation/Validator.java
@@ -0,0 +1,100 @@
+package lotto.validation;
+
+import lotto.domain.ErrorMessage;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.domain.ErrorMessage.*;
+
+public class Validator {
+
+    private static final String BLANK = " ";
+    private static final String NON_BLANK = "";
+    private static final int STANDARD_PRICE = 1_000;
+
+    public int validPrice(String price) {
+        String removeBlank = getRemoveBlank(price);
+        int resultPrice = checkType(removeBlank);
+        checkPrice(resultPrice);
+
+        return resultPrice;
+    }
+
+    public String validWinning(String winning) {
+        String removeBlank = getRemoveBlank(winning);
+        List<String> numbers = Arrays.stream(removeBlank.split(",")).collect(Collectors.toList());
+        numbers.forEach(number -> checkValid(number));
+        return removeBlank;
+    }
+
+    public int validBonus(String bonus, List<Integer> winningNumbers) {
+        String removeBlank = getRemoveBlank(bonus);
+        int resultBonus = checkType(removeBlank);
+        checkNumber(bonus, winningNumbers);
+
+        return resultBonus;
+    }
+
+    private void checkNumber(String bonus, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(bonus)) {
+            getMessage(DISTINCT_ERROR);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkDistinct(List<String> numbers) {
+        List<String> distinctNumbers = numbers.stream().distinct().collect(Collectors.toList());
+        if (numbers.size() != distinctNumbers.size()) {
+            getMessage(SIZE_ERROR);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkValid(String number) {
+        int resultNumber = checkType(number);
+        checkSize(resultNumber);
+    }
+
+    private void checkSize(int resultNumber) {
+        if (!isLottoRange(resultNumber)) {
+            getMessage(LOTTO_NUMBER_ERROR);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isLottoRange(int resultNumber) {
+        return resultNumber >= 1 && resultNumber <= 45;
+    }
+
+    public int validBonus(String bonus) {
+        String removeBlank = getRemoveBlank(bonus);
+        int resultPrice = checkType(removeBlank);
+
+        return resultPrice;
+    }
+
+    private void checkPrice(int resultPrice) {
+        if (resultPrice % STANDARD_PRICE != 0) {
+            getMessage(COMMON_ERROR);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private String getRemoveBlank(String input) {
+        return input.replaceAll(BLANK, NON_BLANK);
+    }
+
+    private int checkType(String price) {
+        int resultPrice;
+        try {
+            resultPrice = Integer.parseInt(price);
+        }catch (NumberFormatException e) {
+            getMessage(COMMON_ERROR);
+            throw new IllegalArgumentException(e);
+        }
+
+        return resultPrice;
+    }
+}
