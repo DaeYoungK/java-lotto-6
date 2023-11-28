171a4f409a1661f4f32decdde206bf5a6f52cd25
DaeYoungK <hanguk830@naver.com>
2023-11-29 01:21:58 +0900

    feat(ErrorMessage): 예외 메시지 추가

--- src/main/java/lotto/domain/ErrorMessage.java
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     COMMON_ERROR("유효한 데이터가 아닙니다. 다시 입력해 주세요."),
     SIZE_ERROR("로또 번호는 6개여야 합니다."),
-    DISTINCT_ERROR("당첨 번호와 중복되면 안됩니다.")
+    DISTINCT_ERROR("당첨 번호와 중복되면 안됩니다."),
+    LOTTO_NUMBER_ERROR("1~45 사이 값만 가능합니다.")
     ;
 
     private static final String ERROR = "[ERROR]";
