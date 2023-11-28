d5296050b2d4c21b4fbb5ebc9edb2170ff08421a
DaeYoungK <hanguk830@naver.com>
2023-11-29 01:44:39 +0900

    feat(Lotto): 검증 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,9 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.domain.ErrorMessage.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,8 +16,17 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            getMessage(SIZE_ERROR);
             throw new IllegalArgumentException();
         }
+        if (isDuplicate(numbers)) {
+            getMessage(DISTINCT_ERROR);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isDuplicate(List<Integer> numbers) {
+        return numbers.stream().distinct().collect(Collectors.toList()).size() != 6;
     }
 
     public static Lotto createLotto(List<Integer> numbers) {
