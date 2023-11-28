df3aa62fa7328981836eec6b2dac1d0804a5664a
DaeYoungK <hanguk830@naver.com>
2023-11-29 00:12:11 +0900

    feat(LottoMachine): 당첨 내역을 출력하는 기능 추가

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -1,27 +1,38 @@
 package lotto.controller;
 
-import lotto.domain.LogForPrint;
-import lotto.domain.Lotto;
-import lotto.domain.LottoStore;
+import lotto.domain.*;
 import lotto.utility.RandomNumber;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoMachine {
 
     private static final int STANDARD_PURCHASE = 1000;
     private static final int ZERO = 0;
+    private int price;
 
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
     private LottoStore lottoStore = new LottoStore();
     private LogForPrint log = new LogForPrint();
+    private Calculator calculator = new Calculator();
+    private WinningNumber winningNumber = new WinningNumber();
 
     public void proceed() {
         int count = payPurchase();
         publishLotto(count);
+        statisticResult();
+    }
+
+    private void statisticResult() {
+        List<Lotto> lottos = lottoStore.getLottos();
+        Map<Statistic, Integer> resultMap = calculator.collectLotto(lottos, winningNumber);
+        double rateOfReturn = calculator.rateOfReturn(price);
+        outputView.printStatistic(log.resultStatistic(resultMap));
+        outputView.printRateOfReturn(log.rateOfReturn(rateOfReturn));
     }
 
     private void publishLotto(int count) {
@@ -34,7 +45,7 @@ public class LottoMachine {
     }
 
     private int payPurchase() {
-        int price = Integer.parseInt(inputView.readPurchase());
+        price = Integer.parseInt(inputView.readPurchase());
         if (price % STANDARD_PURCHASE != ZERO) {
             System.out.println("예외 발생!");
         }
